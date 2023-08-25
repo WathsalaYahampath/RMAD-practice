a3c88e909bb3761928a5369605994cf3369eab73
WathsalaYahampath <yahampa-se19025@stu.kln.ac.lk>
2023-08-25 23:19:14 +0530

    routings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import HomeComponent from "./pages/Home/HomeComponent";
 import AboutComponent from "./pages/About/AboutComponent"; 
 import Home02Component from "./pages/Home02/Home02Component";
+import Navbar from "./components/Navigation/Navbar";
+import {Route,Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div >
     
-     <Home02Component/>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home02Component/>}/>
+      <Route path="/about" element={<AboutComponent/>}/>
+    </Routes>
+     {/* <Home02Component/> */}
     </div>
   );
 }
